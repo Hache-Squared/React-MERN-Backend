549dc3bc041c84ac4e866c360b83850ccbc8f272
Angel <aghh.oficial@gmail.com>
2024-06-22 20:17:45 -0600

    added a log to see error in DB

--- database/config.js
+++ database/config.js
@@ -14,7 +14,7 @@ const dbConnection = async() => {
         console.log('DB Online');
     } catch (error) {
         console.log(error);
-        throw new Error('Error al inicializar BD')
+        throw new Error('Error al inicializar BD: ' + error)
     }
 }
 
